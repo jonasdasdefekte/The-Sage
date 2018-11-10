ce3cb8597cb256403448e955620730bd9263d386
Dustin <dustin.kristen2000@yahoo.de>
2018-11-10 16:35:56 +0100

    How To Amuse Snecko's new description

--- src/main/java/sagemod/cards/HowToAmuseSneckos.java
+++ src/main/java/sagemod/cards/HowToAmuseSneckos.java
@@ -5,7 +5,6 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.FrailPower;
 
 public class HowToAmuseSneckos extends AbstractSageCard {
 
