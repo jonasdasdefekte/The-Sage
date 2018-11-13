25f7dec45e867822e7b16f40e63ac2faa561d66a
jonasdasdefekte <38809615+jonasdasdefekte@users.noreply.github.com>
2018-11-14 01:07:38 +0200

    Folded Carpet

--- src/main/java/sagemod/cards/FoldedCarpet.java
+++ src/main/java/sagemod/cards/FoldedCarpet.java
@@ -17,8 +17,8 @@ public class FoldedCarpet extends AbstractSageCard {
 	private static final CardRarity RARITY = CardRarity.RARE;
 	private static final CardTarget TARGET = CardTarget.SELF;
 
-	private static final int BLOCK_AMT = 18;
-	private static final int UPGRADE_BLOCK_AMT = 6;
+	private static final int BLOCK_AMT = 15;
+	private static final int UPGRADE_BLOCK_AMT = 3;
 
 	public FoldedCarpet() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
