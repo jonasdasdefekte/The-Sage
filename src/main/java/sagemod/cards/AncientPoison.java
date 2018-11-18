c893993b95a0bb20cdc8305ba9934ba4086863c2
Dustin <dustin.kristen2000@yahoo.de>
2018-11-18 17:45:16 +0100

    Changed Ancient Poison
    Instead of applying a flat amount of poison to the enemies, they now
    gain poison equal to their artifact. lLso, the upgrade guarantess
    atleast 5 poison

--- src/main/java/sagemod/cards/AncientPoison.java
+++ src/main/java/sagemod/cards/AncientPoison.java
@@ -6,8 +6,8 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.powers.PoisonPower;
-
 import sagemod.powers.AncientPoisonPower;
 
 public class AncientPoison extends AbstractSageCard {
@@ -19,21 +19,23 @@ public class AncientPoison extends AbstractSageCard {
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final CardType TYPE = CardType.POWER;
 	private static final CardRarity RARITY = CardRarity.RARE;
-	private static final CardTarget TARGET = CardTarget.SELF;
+	private static final CardTarget TARGET = CardTarget.ALL;
 
-	private static final int POISON_AMT = 4;
-	private static final int UPGRADE_POISON_AMT = 3;
+	private static final int ADDITIONAL_POISON_AMT = 5;
 
 	public AncientPoison() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-		baseMagicNumber = magicNumber = POISON_AMT;
+		baseMagicNumber = magicNumber = 0;
 	}
 
 	@Override
 	public void upgrade() {
 		if (!upgraded) {
 			upgradeName();
-			upgradeMagicNumber(UPGRADE_POISON_AMT);
+			upgradeMagicNumber(ADDITIONAL_POISON_AMT);
+
+			rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+			initializeDescription();
 		}
 	}
 
@@ -45,12 +47,18 @@ public class AncientPoison extends AbstractSageCard {
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
 		if (!p.hasPower(AncientPoisonPower.POWER_ID)) {
-			applyPowerToSelf(new AncientPoisonPower(p, 1));
+			applyPowerToSelf(new AncientPoisonPower(p, -1));
 		} else {
 			p.getPower(AncientPoisonPower.POWER_ID).flash();
 		}
 		for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
-			applyPower(new PoisonPower(mo, p, magicNumber), mo);
+			int amount = magicNumber;
+			if (mo.hasPower(ArtifactPower.POWER_ID)) {
+				amount += mo.getPower(ArtifactPower.POWER_ID).amount;
+			}
+			if (amount > 0) {
+				applyPower(new PoisonPower(mo, p, amount), mo);
+			}
 		}
 	}
 
