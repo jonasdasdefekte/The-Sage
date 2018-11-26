7d3facd2265d62407f125beb296340f59dd38586
jonasdasdefekte <38809615+jonasdasdefekte@users.noreply.github.com>
2018-11-27 00:56:09 +0200

    Create GroundedStrike.java

---
+++ src/main/java/sagemod/cards/GroundedStrike.java
@@ -0,0 +1,70 @@
+package sagemod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import sagemod.actions.ReduceFlightBlockableByArtifactAction;
+import sagemod.powers.SageFlight;
+
+public class GroundedStrike extends AbstractSageCard {
+
+	public static final String ID = "Grounded_Strike";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	private static final int COST = 1;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+	private static final CardType TYPE = CardType.ATTACK;
+	private static final CardRarity RARITY = CardRarity.COMMON;
+	private static final CardTarget TARGET = CardTarget.ENEMY;
+
+	private static final int ATTACK_DMG = 10;
+	private static final int UPGRADE_ATTACK_DMG = 3;
+	private static final int FLIGHT_LOSS = 1;
+
+	public GroundedStrike() {
+		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
+		baseDamage = ATTACK_DMG;
+		baseMagicNumber = magicNumber = FLIGHT_LOSS;
+
+		tags.add(CardTags.STRIKE);
+	}
+
+	@Override
+	public void upgrade() {
+		if (!upgraded) {
+			upgradeName();
+			upgradeDamage(UPGRADE_ATTACK_DMG);
+			rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+			initializeDescription();
+		}
+	}
+
+	@Override
+	public AbstractCard makeCopy() {
+		return new GroundedStrike();
+	}
+
+	@Override
+	public void use(AbstractPlayer p, AbstractMonster m) {
+		attack(m, AttackEffect.SLASH_HORIZONTAL);
+		if (!upgraded) {
+			if (p.hasPower(SageFlight.POWER_ID)) {
+				AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(p, p, SageFlight.POWER_ID));
+			}
+		}
+		else {
+			AbstractDungeon.actionManager.addToBottom(new ReduceFlightBlockableByArtifactAction(magicNumber, p));
+		}
+	}
+
+	@Override
+	public String getLoadedDescription() {
+		return DESCRIPTION;
+	}
+
+}
