134ab0ff2ad01e4364345d6e150f3454ccd64c81
Dustin <dustin.kristen2000@yahoo.de>
2018-12-09 19:48:56 +0100

    Changed: How To Murder Ants
    Now does X damage 6 (8) times.

--- src/main/java/sagemod/cards/HowToMurderAnts.java
+++ src/main/java/sagemod/cards/HowToMurderAnts.java
@@ -20,23 +20,21 @@ public class HowToMurderAnts extends AbstractSageCard {
 	private static final CardRarity RARITY = CardRarity.UNCOMMON;
 	private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
 
-	private static final int ATTACK_DMG = 8;
-	private static final int BASE_ADDED_EFFECT = 0;
-	private static final int UPGRADE_ADDED_EFFECT = 1;
+	private static final int BASE_ATTACK_DMG = 0;
+	private static final int BASE_TIMES = 6;
+	private static final int UPGRADE_TIMES = 2;
 
 	public HowToMurderAnts() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-		baseMagicNumber = magicNumber = BASE_ADDED_EFFECT;
-		baseDamage = ATTACK_DMG;
+		baseMagicNumber = magicNumber = BASE_TIMES;
+		baseDamage = BASE_ATTACK_DMG;
 	}
 
 	@Override
 	public void upgrade() {
 		if (!upgraded) {
 			upgradeName();
-			upgradeMagicNumber(UPGRADE_ADDED_EFFECT);
-			rawDescription = cardStrings.UPGRADE_DESCRIPTION;
-			initializeDescription();
+			upgradeMagicNumber(UPGRADE_TIMES);
 		}
 	}
 
@@ -48,10 +46,11 @@ public class HowToMurderAnts extends AbstractSageCard {
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
 		if (isFlying()) {
-			int effect = getXEffect() + magicNumber;
+			int effect = getXEffect();
 			if (effect > 0) {
 				AbstractDungeon.actionManager.addToBottom(new SwordBoomerangAction(
-						AbstractDungeon.getMonsters().getRandomMonster(true), new DamageInfo(p, baseDamage), effect));
+						AbstractDungeon.getMonsters().getRandomMonster(true),
+						new DamageInfo(p, effect), magicNumber));
 				useXEnergy();
 			}
 		}
