ad98d570886ff99ad28c29ada0a7912eedf8bc3d
Dustin <dustin.kristen2000@yahoo.de>
2018-12-23 04:39:34 +0100

    Changed Toy Ornithopter
    This relic now heals the sage when a potion is discarded, not when one
    is used

---
+++ The-Sage/src/main/java/sagemod/patches/ToyOrnithopterPatches.java
@@ -0,0 +1,61 @@
+package sagemod.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.ToyOrnithopter;
+import com.megacrit.cardcrawl.ui.panels.PotionPopUp;
+import sagemod.character.TheSage;
+
+public class ToyOrnithopterPatches {
+
+	public static final String RELIC_ID = "sagemod:Toy_Ornithopter";
+
+	@SpirePatch(clz = ToyOrnithopter.class, method = "getUpdatedDescription")
+	public static class DescriptionPatch {
+		public static String Replace(ToyOrnithopter relic) {
+			if (AbstractDungeon.player instanceof TheSage) {
+				String[] DESCRIPTIONS =
+						CardCrawlGame.languagePack.getRelicStrings(RELIC_ID).DESCRIPTIONS;
+				return DESCRIPTIONS[0] + ToyOrnithopter.HEAL_AMT + DESCRIPTIONS[1];
+			} else {
+				return relic.DESCRIPTIONS[0] + ToyOrnithopter.HEAL_AMT + relic.DESCRIPTIONS[1];
+			}
+		}
+	}
+
+	@SpirePatch(clz = ToyOrnithopter.class, method = "onUsePotion")
+	public static class UsePotionPatch {
+		public static void Replace(ToyOrnithopter relic) {
+			if (!(AbstractDungeon.player instanceof TheSage)) {
+				relic.flash();
+				AbstractDungeon.actionManager
+				.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, relic));
+				AbstractDungeon.actionManager.addToBottom(
+						new HealAction(AbstractDungeon.player, AbstractDungeon.player,
+								ToyOrnithopter.HEAL_AMT));
+			}
+		}
+	}
+
+	@SpirePatch(clz = PotionPopUp.class, method = "updateInput")
+	public static class DiscardPotionPatch {
+		@SpireInsertPatch(rloc = 57)
+		public static void Insert(PotionPopUp popup) {
+			if (AbstractDungeon.player.hasRelic(ToyOrnithopter.ID)
+					&& AbstractDungeon.player instanceof TheSage) {
+				ToyOrnithopter relic =
+						(ToyOrnithopter) AbstractDungeon.player.getRelic(ToyOrnithopter.ID);
+				relic.flash();
+				AbstractDungeon.actionManager
+				.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, relic));
+				AbstractDungeon.actionManager.addToBottom(
+						new HealAction(AbstractDungeon.player, AbstractDungeon.player,
+								ToyOrnithopter.HEAL_AMT));
+			}
+		}
+	}
+}
