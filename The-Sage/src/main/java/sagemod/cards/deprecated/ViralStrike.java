6a6aa6bb1873169c43753ce3973df532dccad83a
Dustin <dustin.kristen2000@yahoo.de>
2018-12-23 16:41:05 +0100

    Fixed Typo

--- The-Sage/src/main/java/sagemod/cards/deprecated/ViralStrike.java
+++ The-Sage/src/main/java/sagemod/cards/deprecated/ViralStrike.java
@@ -20,7 +20,7 @@ public class ViralStrike extends AbstractSageCard {
 	private static final int COST = 2;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final CardType TYPE = CardType.ATTACK;
-	private static final CardRarity RARITY = CardRarity.SPECIAl;
+	private static final CardRarity RARITY = CardRarity.SPECIAL;
 	private static final CardTarget TARGET = CardTarget.ENEMY;
 
 	private static final int ATTACK_DMG = 6;
