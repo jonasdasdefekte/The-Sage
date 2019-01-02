09f1ae6f192eb5a7fb7b68ef836a09c75212f6f3
Dustin <dustin.kristen2000@yahoo.de>
2019-01-02 15:37:25 +0100

    Changed Anti Ancient Attack

--- The-Sage/src/main/java/sagemod/cards/AntiAncientAttack.java
+++ The-Sage/src/main/java/sagemod/cards/AntiAncientAttack.java
@@ -18,18 +18,21 @@ public class AntiAncientAttack extends AbstractSageCard {
 	public static final String ID = "Anti_Ancient_Attack";
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
-	private static final int COST = 3;
+	private static final int COST = 1;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final CardType TYPE = CardType.ATTACK;
 	private static final CardRarity RARITY = CardRarity.RARE;
 	private static final CardTarget TARGET = CardTarget.ENEMY;
 
-	private static final int ATTACK_DMG = 10;
-	private static final int UPGRADE_ATTACK_DMG = 5;
+	private static final int ATTACK_DMG = 6;
+	private static final int UPGRADE_ATTACK_DMG = 2;
+	private static final int ARTIFACT_GAIN = 1;
+	private static final int UPGRADE_ARTIFACT_GAIN = 1;
 
 	public AntiAncientAttack() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
 		baseDamage = ATTACK_DMG;
+		baseMagicNumber = magicNumber = ARTIFACT_GAIN;
 	}
 
 	@Override
@@ -37,6 +40,7 @@ public class AntiAncientAttack extends AbstractSageCard {
 		if (!upgraded) {
 			upgradeName();
 			upgradeDamage(UPGRADE_ATTACK_DMG);
+			upgradeMagicNumber(UPGRADE_ARTIFACT_GAIN);
 		}
 	}
 
@@ -47,14 +51,10 @@ public class AntiAncientAttack extends AbstractSageCard {
 
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		if (m.hasPower(ArtifactPower.POWER_ID)) {
-			applyPower(new ArtifactPower(m, artifactAmount(m)), m);
-		}
+		applyPower(new ArtifactPower(m, magicNumber), m);
 		AbstractDungeon.actionManager.addToBottom(new ExecuteLaterAction(() -> {
-			int times = artifactAmount(p);
-			for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
-				times += artifactAmount(mo);
-			}
+			int times = 0;
+			times += artifactAmount(m);
 			for (int i = 0; i < times; i++) {
 				AbstractDungeon.actionManager.addToBottom(new DamageAction(m,
 						new DamageInfo(player(), damage, damageTypeForTurn), AttackEffect.SMASH,
@@ -71,13 +71,9 @@ public class AntiAncientAttack extends AbstractSageCard {
 	}
 
 	private void updateExtendedDescription(AbstractMonster m) {
-		int times = artifactAmount(AbstractDungeon.player);
-		for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
-			int amt = artifactAmount(mo);
-			if (mo == m) {
-				amt *= 2;
-			}
-			times += amt;
+		int times = magicNumber;
+		if (m != null) {
+			times += artifactAmount(m);
 		}
 		rawDescription = getLoadedDescription();
 		if (times == 1) {
