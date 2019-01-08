efcff7f7d92377f08bae976bcb5308cca8200cff
Dustin <dustin.kristen2000@yahoo.de>
2019-01-08 18:46:33 +0100

    Fix Brew Rewards not being cleared

--- The-Sage/src/main/java/sagemod/patches/PatchesForBrewScreen.java
+++ The-Sage/src/main/java/sagemod/patches/PatchesForBrewScreen.java
@@ -1,12 +1,17 @@
 package sagemod.patches;
 
+import java.util.ArrayList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import com.evacipated.cardcrawl.modthespire.lib.LineFinder;
+import com.evacipated.cardcrawl.modthespire.lib.Matcher;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon.CurrentScreen;
 import com.megacrit.cardcrawl.ui.buttons.CancelButton;
+import javassist.CtBehavior;
 import sagemod.powers.Brew;
 
 public class PatchesForBrewScreen {
@@ -15,12 +20,20 @@ public class PatchesForBrewScreen {
 	@SpirePatch(clz = CancelButton.class, method = "update")
 	public static class CancelButtonPatches {
 
-		@SpireInsertPatch(rloc = 41)
+		@SpireInsertPatch(locator = Locator.class)
 		public static void Insert(Object o) {
 			if (AbstractDungeon.screen == CurrentScreen.COMBAT_REWARD && Brew.isBrewRewards) {
 				Brew.isBrewRewards = false;
 				logger.info("Closing Brew Rewards and deleting excess potions");
 				Brew.removePotionsFromRewards();
+			}
+		}
+
+		private static class Locator extends SpireInsertLocator {
+			@Override
+			public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+				return LineFinder.findAllInOrder(ctMethodToPatch, new ArrayList<Matcher>(),
+						new Matcher.MethodCallMatcher(CancelButton.class, "hide"));
 			}
 		}
 	}
