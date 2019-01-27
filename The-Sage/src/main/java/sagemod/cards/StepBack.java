2937ac34f46ab1f784876c5dcc3e11db58f0d5fe
Dustin <dustin.kristen2000@yahoo.de>
2019-01-27 10:34:36 +0100

    Changed Energy Gain on Step Back to Refund

--- The-Sage/src/main/java/sagemod/cards/StepBack.java
+++ The-Sage/src/main/java/sagemod/cards/StepBack.java
@@ -1,5 +1,6 @@
 package sagemod.cards;
 
+import com.evacipated.cardcrawl.mod.stslib.variables.RefundVariable;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -21,12 +22,13 @@ public class StepBack extends AbstractSageCard {
 	private static final int BLOCK_AMT = 5;
 	private static final int UPGRADE_BLOCK_AMT = 3;
 	private static final int FLIGHT_AMT = 1;
-	private static final int ENERGY_GAIN = 2;
+	private static final int REFUND_AMOUNT = 2;
 
 	public StepBack() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
 		baseBlock = BLOCK_AMT;
 		baseMagicNumber = magicNumber = FLIGHT_AMT;
+		initSageMisc(REFUND_AMOUNT);
 	}
 
 	@Override
@@ -46,8 +48,9 @@ public class StepBack extends AbstractSageCard {
 	public void use(AbstractPlayer p, AbstractMonster m) {
 		block();
 		if (isFlying()) {
-			gainEnergy(ENERGY_GAIN);
+			RefundVariable.setBaseValue(this, REFUND_AMOUNT);
 		} else {
+			RefundVariable.setBaseValue(this, 0);
 			applyPowerToSelf(new Flight(p, magicNumber));
 		}
 	}
