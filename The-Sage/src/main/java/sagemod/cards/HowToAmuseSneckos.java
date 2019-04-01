4a7079841d43d794a6276dccfa5c0ce14a575dc5
Dustin <skrelpoid@protonmail.com>
2019-04-01 19:58:18 +0200

    Changed How To Amuse Sneckos
    Now you lose Flight but gain more refund

--- The-Sage/src/main/java/sagemod/cards/HowToAmuseSneckos.java
+++ The-Sage/src/main/java/sagemod/cards/HowToAmuseSneckos.java
@@ -4,8 +4,12 @@ import com.evacipated.cardcrawl.mod.stslib.variables.RefundVariable;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import sagemod.actions.ReduceFlightBlockableAction;
+import sagemod.powers.Flight;
 
 public class HowToAmuseSneckos extends AbstractSageCard {
 
@@ -18,20 +22,25 @@ public class HowToAmuseSneckos extends AbstractSageCard {
 	private static final CardRarity RARITY = CardRarity.UNCOMMON;
 	private static final CardTarget TARGET = CardTarget.SELF;
 
-	private static final int REFUND_AMT = 1;
-	private static final int UPGRADE_REFUND_AMT = 1;
+	private static final int REFUND_AMT = 3;
+	private static final int UPGRADE_REFUND_AMT = 3;
+	private static final int FLIGHT_LOSS = 999;
+	private static final int UPGRADE_FLIGHT_LOSS = -998;
 
 	public HowToAmuseSneckos() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
 		RefundVariable.setBaseValue(this, REFUND_AMT);
+		baseMagicNumber = magicNumber = FLIGHT_LOSS;
 	}
 
 	@Override
 	public void upgrade() {
 		if (!upgraded) {
 			upgradeName();
-
 			RefundVariable.upgrade(this, UPGRADE_REFUND_AMT);
+			upgradeMagicNumber(UPGRADE_FLIGHT_LOSS);
+			rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+			initializeDescription();
 		}
 	}
 
@@ -43,6 +52,11 @@ public class HowToAmuseSneckos extends AbstractSageCard {
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
 		int effect = getXEffect();
+		
+		if (p.hasPower(Flight.POWER_ID)) {
+			AbstractDungeon.actionManager.addToBottom(
+					new ReduceFlightBlockableAction(magicNumber, AbstractDungeon.player));
+		}
 
 		int draw = effect;
 		if (draw > 0) {
@@ -54,7 +68,7 @@ public class HowToAmuseSneckos extends AbstractSageCard {
 
 	@Override
 	public String getLoadedDescription() {
-		return DESCRIPTION;
+		return upgraded ? cardStrings.UPGRADE_DESCRIPTION : DESCRIPTION;
 	}
 
 }
