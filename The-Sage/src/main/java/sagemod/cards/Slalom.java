303777397c4a9335dfbd0a6695e914cfc66b2e71
Dustin <skrelpoid@protonmail.com>
2020-02-18 19:42:39 +0100

    Changed Slalom
    Now just sets Fight to 2 (4)

--- The-Sage/src/main/java/sagemod/cards/Slalom.java
+++ The-Sage/src/main/java/sagemod/cards/Slalom.java
@@ -1,11 +1,14 @@
 package sagemod.cards;
 
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.FrailPower;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import sagemod.powers.Flight;
 
 public class Slalom extends AbstractSageCard {
 
@@ -18,21 +21,19 @@ public class Slalom extends AbstractSageCard {
 	private static final CardRarity RARITY = CardRarity.COMMON;
 	private static final CardTarget TARGET = CardTarget.SELF;
 
-	private static final int BLOCK_AMT = 10;
-	private static final int UPGRADE_BLOCK_AMT = 3;
-	private static final int FRAIL_GAIN = 2;
+	private static final int BASE_FLIGHT_AMT = 2;
+	private static final int UPGRADE_FLIGHT_AMT = 2;
 
 	public Slalom() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-		baseBlock = BLOCK_AMT;
-		baseMagicNumber = magicNumber = FRAIL_GAIN;
+		baseMagicNumber = magicNumber = BASE_FLIGHT_AMT;
 	}
 
 	@Override
 	public void upgrade() {
 		if (!upgraded) {
 			upgradeName();
-			upgradeBlock(UPGRADE_BLOCK_AMT);
+			upgradeMagicNumber(UPGRADE_FLIGHT_AMT);
 		}
 	}
 
@@ -43,8 +44,18 @@ public class Slalom extends AbstractSageCard {
 
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		block();
-		applyPowerToSelf(new FrailPower(p, magicNumber, false));
+		if (isFlying()) {
+			AbstractPower power = p.getPower(Flight.POWER_ID);
+			if (magicNumber <= 0) {
+				AbstractDungeon.actionManager
+						.addToBottom(new RemoveSpecificPowerAction(p, p, power));
+			} else {
+				power.amount = magicNumber;
+				power.flash();
+			}
+		} else if (magicNumber > 0) {
+			applyPowerToSelf(new Flight(p, magicNumber));
+		}
 	}
 
 	@Override
