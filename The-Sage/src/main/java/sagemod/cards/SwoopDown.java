bb54c8cc27bb462ac696c167189978b120cbf89b
Dustin <skrelpoid@protonmail.com>
2020-02-18 20:13:17 +0100

    Changed Swoop Down
    Target Random Monster, Lose all your flight

--- The-Sage/src/main/java/sagemod/cards/SwoopDown.java
+++ The-Sage/src/main/java/sagemod/cards/SwoopDown.java
@@ -22,14 +22,12 @@ public class SwoopDown extends AbstractSageCard {
 	private static final CardRarity RARITY = CardRarity.RARE;
 	private static final CardTarget TARGET = CardTarget.ENEMY;
 
-	private static final int ATTACK_DMG = 4;
-	private static final int UPGRADE_ATTACK_DMG = 2;
-	private static final int LOSE_FLIGHT = 1;
+	private static final int ATTACK_DMG = 7;
+	private static final int UPGRADE_ATTACK_DMG = 3;
 
 	public SwoopDown() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
 		baseDamage = ATTACK_DMG;
-		baseMagicNumber = magicNumber = LOSE_FLIGHT;
 	}
 
 	@Override
@@ -48,54 +46,20 @@ public class SwoopDown extends AbstractSageCard {
 	}
 
 	@Override
-	public void applyPowers() {
-		super.applyPowers();
-		updateExtendedDescription();
-	}
-
-	@Override
-	public void calculateCardDamage(AbstractMonster mo) {
-		super.calculateCardDamage(mo);
-		updateExtendedDescription();
-	}
-
-	private void updateExtendedDescription() {
-		int count = 0;
-		if (player().hasPower(Flight.POWER_ID)) {
-			count = player().getPower(Flight.POWER_ID).amount;
-		}
-		rawDescription = getLoadedDescription();
-		rawDescription =
-				rawDescription + EXTENDED_DESCRIPTION[0] + damage * count + EXTENDED_DESCRIPTION[1];
-		initializeDescription();
-	}
-
-	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
 		int times = 0;
 		if (isFlying()) {
 			times = player().getPower(Flight.POWER_ID).amount;
 		}
 		for (int i = 0; i < times; i++) {
-			attack(m, AttackEffect.SLASH_VERTICAL);
+			attack(AbstractDungeon.getCurrRoom().monsters.getRandomMonster(true), AttackEffect.SLASH_VERTICAL);
 		}
-		if (!upgraded) {
-			AbstractDungeon.actionManager
-					.addToBottom(new ReduceFlightBlockableAction(magicNumber, p));
-		}
-		rawDescription = getLoadedDescription();
-		initializeDescription();
-	}
-
-	@Override
-	public void onMoveToDiscard() {
-		rawDescription = getLoadedDescription();
-		initializeDescription();
+		AbstractDungeon.actionManager.addToBottom(new ReduceFlightBlockableAction(999, p));
 	}
 
 	@Override
 	public String getLoadedDescription() {
-		return upgraded ? cardStrings.UPGRADE_DESCRIPTION : DESCRIPTION;
+		return DESCRIPTION;
 	}
 
 }
