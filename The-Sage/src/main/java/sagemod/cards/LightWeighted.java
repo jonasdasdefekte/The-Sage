7f72d0295f0f358c6303f7fbbef21a0ed8d3b52b
Dustin <skrelpoid@protonmail.com>
2020-02-18 20:20:10 +0100

    Changed LightWeighted
    Lose Block instead of Frail, gain half or upgraded all as flight

--- The-Sage/src/main/java/sagemod/cards/LightWeighted.java
+++ The-Sage/src/main/java/sagemod/cards/LightWeighted.java
@@ -1,13 +1,12 @@
 package sagemod.cards;
 
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.LoseBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.FrailPower;
 import sagemod.powers.Flight;
 
 public class LightWeighted extends AbstractSageCard {
@@ -21,12 +20,12 @@ public class LightWeighted extends AbstractSageCard {
 	private static final CardRarity RARITY = CardRarity.RARE;
 	private static final CardTarget TARGET = CardTarget.SELF;
 
-	private static final int EXTRA_FLIGHT = 0;
-	private static final int UPGRADE_EXTRA_FLIGHT = 1;
-
+	private static final int DIVIDE_FLIGHT_BY = 2;
+	private static final int UPGRADE_DIVIDE_FLIGHT_BY = -1;
+	
 	public LightWeighted() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
-		baseMagicNumber = magicNumber = EXTRA_FLIGHT;
+		baseMagicNumber = magicNumber = DIVIDE_FLIGHT_BY;
 		exhaust = true;
 	}
 
@@ -34,7 +33,7 @@ public class LightWeighted extends AbstractSageCard {
 	public void upgrade() {
 		if (!upgraded) {
 			upgradeName();
-			upgradeMagicNumber(UPGRADE_EXTRA_FLIGHT);
+			upgradeMagicNumber(UPGRADE_DIVIDE_FLIGHT_BY);
 			rawDescription = cardStrings.UPGRADE_DESCRIPTION;
 			initializeDescription();
 		}
@@ -47,12 +46,9 @@ public class LightWeighted extends AbstractSageCard {
 
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		int flightGain = magicNumber;
-		if (p.hasPower(FrailPower.POWER_ID)) {
-			flightGain += p.getPower(FrailPower.POWER_ID).amount;
-			AbstractDungeon.actionManager
-					.addToBottom(new RemoveSpecificPowerAction(p, p, FrailPower.POWER_ID));
-		}
+		int block = player().currentBlock;
+		AbstractDungeon.actionManager.addToBottom(new LoseBlockAction(p, p, 999));
+		int flightGain = block / magicNumber;
 		if (flightGain > 0) {
 			applyPowerToSelf(new Flight(p, flightGain));
 		}
@@ -60,6 +56,6 @@ public class LightWeighted extends AbstractSageCard {
 
 	@Override
 	public String getLoadedDescription() {
-		return DESCRIPTION;
+		return upgraded ? cardStrings.UPGRADE_DESCRIPTION : DESCRIPTION;
 	}
 }
