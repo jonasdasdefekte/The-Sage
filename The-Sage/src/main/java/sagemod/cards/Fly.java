89f3a900e48a33eb154d3771cb48ee2bc92d9852
Dustin <skrelpoid@protonmail.com>
2020-02-25 18:54:58 +0100

    Changed Fly:
    Base Cost 1: Upgrade doesnt lower cost but ups defense

--- The-Sage/src/main/java/sagemod/cards/Fly.java
+++ The-Sage/src/main/java/sagemod/cards/Fly.java
@@ -12,14 +12,14 @@ public class Fly extends AbstractSageCard {
 	public static final String ID = "sagemod:Fly";
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
-	private static final int COST = 2;
+	private static final int COST = 1;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final CardType TYPE = CardType.SKILL;
 	private static final CardRarity RARITY = CardRarity.BASIC;
 	private static final CardTarget TARGET = CardTarget.SELF;
 
-	private static final int BLOCK_AMT = 10;
-	private static final int COST_WHEN_UPGRADED = 1;
+	private static final int BLOCK_AMT = 7;
+	private static final int UPGRADE_BLOCK_BY = 3;
 	private static final int FLIGHT_AMT = 1;
 
 
@@ -33,7 +33,7 @@ public class Fly extends AbstractSageCard {
 	public void upgrade() {
 		if (!upgraded) {
 			upgradeName();
-			upgradeBaseCost(COST_WHEN_UPGRADED);
+			upgradeBlock(UPGRADE_BLOCK_BY);
 		}
 	}
 
