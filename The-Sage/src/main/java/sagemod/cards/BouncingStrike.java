d9e155bf06da3bf6745a77f00555e6cde6b60274
Dustin <skrelpoid@protonmail.com>
2020-03-15 13:44:47 +0100

    Better Implementation for Bouncing Strike

--- The-Sage/src/main/java/sagemod/cards/BouncingStrike.java
+++ The-Sage/src/main/java/sagemod/cards/BouncingStrike.java
@@ -4,10 +4,8 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import sagemod.actions.ExecuteLaterAction;
 import sagemod.powers.Flight;
 
 public class BouncingStrike extends AbstractSageCard {
@@ -19,7 +17,7 @@ public class BouncingStrike extends AbstractSageCard {
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final CardType TYPE = CardType.ATTACK;
 	private static final CardRarity RARITY = CardRarity.UNCOMMON;
-	private static final CardTarget TARGET = CardTarget.ENEMY;
+	private static final CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
 
 	private static final int FLIGHT_GAIN = 1;
 	private static final int BASE_DMG_TIMES = 3;
@@ -39,7 +37,6 @@ public class BouncingStrike extends AbstractSageCard {
 		if (!upgraded) {
 			upgradeName();
 			upgradeSageMisc(UPGRADE_DMG_TIMES);
-			updateDamage(null);
 		}
 	}
 
@@ -51,21 +48,46 @@ public class BouncingStrike extends AbstractSageCard {
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
 		applyPowerToSelf(new Flight(p, magicNumber));
-		AbstractDungeon.actionManager.addToBottom(new ExecuteLaterAction(() -> {
-			updateDamage(m);
-			attack(m, AttackEffect.SMASH);
-		}));
+		applyPowers();
+		calculateCardDamage(m);
+		attack(m, AttackEffect.SLASH_DIAGONAL);
+		rawDescription = DESCRIPTION;
+		initializeDescription();
 	}
 
-	private void updateDamage(AbstractMonster m) {
-		int damageAmount = sageMisc;
-		if (player().hasPower(Flight.POWER_ID)) {
-			damageAmount *= player().getPower(Flight.POWER_ID).amount;
+	@Override
+	public void applyPowers() {
+		addFlightDamage();
+		super.applyPowers();
+	}
+	
+	private void addFlightDamage() {
+		int amount = magicNumber;
+		if (isFlying()) {
+			amount += player().getPower(Flight.POWER_ID).amount;
 		}
-		baseDamage = damageAmount;
-		calculateCardDamage(m);
-		rawDescription = getLoadedDescription();
+		baseDamage = amount * sageMisc;
+		damage = baseDamage;
+		rawDescription = DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
 		initializeDescription();
+	}
+	
+	@Override
+	public void onMoveToDiscard() {
+		rawDescription = DESCRIPTION;
+		initializeDescription();
+	}
+
+	@Override
+	public void calculateDamageDisplay(AbstractMonster mo) {
+		addFlightDamage();
+		super.calculateDamageDisplay(mo);
+	}
+	
+	@Override
+	public void calculateCardDamage(AbstractMonster mo) {
+		addFlightDamage();
+		super.calculateCardDamage(mo);
 	}
 
 	@Override
