ca9705f38ea6a1c8efd6007a41a285fbbddeed00
jonasdasdefekte <38809615+jonasdasdefekte@users.noreply.github.com>
2021-10-13 14:16:11 +0200

    Swoop down
    Unnerfing Swoop down in another way.
    Cost back to 1, but only lose 1 Flight (none with upgrade)
    I tried reverting parts of the code to pre-2020 but I don't know how to make the upgraded description appear

--- The-Sage/src/main/java/sagemod/cards/SwoopDown.java
+++ The-Sage/src/main/java/sagemod/cards/SwoopDown.java
@@ -15,26 +15,26 @@ public class SwoopDown extends AbstractSageCard {
 	public static final String ID = "sagemod:Swoop_Down";
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
-	private static final int COST = 0;
+	private static final int COST = 1;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
 	private static final CardType TYPE = CardType.ATTACK;
 	private static final CardRarity RARITY = CardRarity.RARE;
 	private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
 
-	private static final int ATTACK_DMG = 7;
-	private static final int UPGRADE_ATTACK_DMG = 3;
+	private static final int ATTACK_DMG = 8;
+	private static final int LOSE_FLIGHT = 1;
 
 	public SwoopDown() {
 		super(ID, NAME, COST, DESCRIPTION, TYPE, RARITY, TARGET);
 		baseDamage = ATTACK_DMG;
+		baseMagicNumber = magicNumber = LOSE_FLIGHT;
 	}
 
 	@Override
 	public void upgrade() {
 		if (!upgraded) {
 			upgradeName();
-			upgradeDamage(UPGRADE_ATTACK_DMG);
 		}
 	}
 
@@ -52,12 +52,14 @@ public class SwoopDown extends AbstractSageCard {
 		for (int i = 0; i < times; i++) {
 			attack(AbstractDungeon.getCurrRoom().monsters.getRandomMonster(true), AttackEffect.SLASH_VERTICAL);
 		}
-		AbstractDungeon.actionManager.addToBottom(new ReduceFlightBlockableAction(999, p));
+		if (!upgraded) {
+		AbstractDungeon.actionManager.addToBottom(new ReduceFlightBlockableAction(magicNumber, p));
+		}
 	}
 
 	@Override
 	public String getLoadedDescription() {
-		return DESCRIPTION;
+ 		return upgraded ? cardStrings.UPGRADE_DESCRIPTION : DESCRIPTION;
 	}
 
 }
